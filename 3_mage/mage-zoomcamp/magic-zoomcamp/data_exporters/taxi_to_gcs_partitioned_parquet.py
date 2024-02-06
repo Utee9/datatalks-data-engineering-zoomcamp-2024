440325d5565c5871185328a360111a433635ac89
Utee <utemma9@gmail.com>
2024-02-06 18:34:30 +0100

    modified gitignore and added mage pipeline and code

---
+++ 3_mage/mage-zoomcamp/magic-zoomcamp/data_exporters/taxi_to_gcs_partitioned_parquet.py
@@ -0,0 +1,34 @@
+import pyarrow as pa
+import pyarrow.parquet as pq
+import os
+
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/home/src/datatalks-de-351308-fcafb3d971e9.json"
+
+bucket_name = 'dtc_data_lake_datatalks-de-351308'
+project_id = 'datatalks-de-351308'
+
+table_name = "nyc_greentaxi"
+
+root_path = f'{bucket_name}/{table_name}'
+
+@data_exporter
+def export_data(data, *args, **kwargs):
+    data['lpep_pickup_datetime'] = data['lpep_pickup_datetime'].dt.date
+    # data['lpep_dropoff_datetime'] = data['lpep_dropoff_datetime'].dt.date
+
+    table = pa.Table.from_pandas(data)
+
+    gcs = pa.fs.GcsFileSystem()
+
+    pq.write_to_dataset(
+        table,
+        root_path=root_path,
+        partition_cols = ['lpep_pickup_datetime'],
+        filesystem=gcs
+    )
+
+
+
---
+++ 3_mage/mage-zoomcamp/magic-zoomcamp/data_loaders/load_taxi_data.sql
@@ -0,0 +1,4 @@
+-- Docs: https://docs.mage.ai/guides/sql-blocks
+SELECT *
+FROM ny_taxi.green_cab_data
+LIMIT 10
